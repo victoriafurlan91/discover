a511a84c04700bb309dcf658ec6361513dc7a6d7
victoriafurlan <victoriafurlan91@gmail.com>
2025-11-25 14:04:56 -0300

    :tada:initial commit

---
+++ script.js
@@ -0,0 +1,18 @@
+function togglemode () {
+    const html = document.documentElement
+    html.classList.toggle("light")
+
+// pegar a tag img
+const img = document.querySelector("#profile img")
+
+//substituir img
+    if (html.classList.contains("light")) {
+// se tiver light mode, adicionar a imagemm light        
+    img.setAttribute("src", "./assets/avatar-light.png")
+    } else {
+// se tiver sem light mode manter imagem normal 
+   
+  img.setAttribute("src","./assets/avatar.png")
+}
+
+}
