4ebd891109098129761c103241761413c4788d87
victoriafurlan <victoriafurlan91@gmail.com>
2025-12-03 14:27:51 -0300

    Update layout: improved light mode styling and hover interactions

--- script.js
+++ script.js
@@ -8,11 +8,11 @@ const img = document.querySelector("#profile img")
 //substituir img
     if (html.classList.contains("light")) {
 // se tiver light mode, adicionar a imagemm light        
-    img.setAttribute("src", "./assets/avatar-light.png")
+    img.setAttribute("src", "./assets/avatarv-light.jpeg")
     } else {
 // se tiver sem light mode manter imagem normal 
    
-  img.setAttribute("src","./assets/avatar.png")
+  img.setAttribute("src","./assets/avatar-viki.png")
 }
 
 }
